627c8a44748543a22a53b4930e3a740f6e5b647e
jiarong <jiarong.xu@ucalgary.ca>
2020-10-26 16:31:00 -0700

    initialize

--- getDirStats.cpp
+++ getDirStats.cpp
@@ -1,18 +1,10 @@
-/// =========================================================================
-/// Written by pfederl@ucalgary.ca in 2020, for CPSC457.
-/// =========================================================================
-/// You need to edit this file.
-///
-/// You can delete all contents of this file and start from scratch if
-/// you wish, but you need to implement the getDirStats() function as
-/// defined in "getDirStats.h".
-
 #include "getDirStats.h"
 #include "digester.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <dirent.h>
 
 static bool
 is_dir( const std::string & path)
@@ -23,45 +15,54 @@ is_dir( const std::string & path)
   return S_ISDIR(buff.st_mode);
 }
 
-// ======================================================================
-// You need to re-implement this function !!!!
-// ======================================================================
-//
 // getDirStats() computes stats about directory dir_name
 // if successful, it return true and stores the results in 'res'
 // on failure, it returns false, and res is in undefined state
-//
+
+//(a) largest file in the directory,
+//(b) size of the largest file,
+//(c) total number of files in the directory,
+//(d) total number of subdirectories,
+//(e) sum of all file sizes,
+//(f) lit of 5 most common file types,
+//(g) list of 5 largest groups of duplicate files
 bool
 getDirStats(const std::string & dir_name, Results & res)
 {
-  // The results below are all hard-coded, to show you all the fields
-  // you need to calculate. You should delete all code below and
-  // replace it with your own code.
+  std::string largest_file_path; // path of the largest file in the directory
+  long largest_file_size = -1; // size of the largest file
+  long n_files = 0; // total number of files in the directory (recursive)
+  long n_dirs = 0; // total number of directories in the directory (recursive)
+  long all_files_size = 0; // cumulative size of all files
+  std::vector<std::string> most_common_types; // 5 most common file types, as reported by the file utility,
+  std::vector<std::vector<std::string>> duplicate_files; // list of groups of duplicate files (top 5)
 
-  if( ! is_dir(dir_name))
-    return false;
 
-  // prepare a fake result
-  res.largest_file_path = dir_name + "/some_dir/some_file.txt";
-  res.largest_file_size = 123;
-  res.n_files = 321;
-  res.n_dirs = 333;
-  res.all_files_size = 1000000;
-  res.most_common_types = {
-    "C source, ASCII text",
-    "makefile script, ASCII text",
-    "C++ source, ASCII text",
-    "directory"
-  };
-  std::vector<std::string> group1;
-  group1.push_back(dir_name + "/file1.cpp");
-  group1.push_back(dir_name + "/lib/sub/other.c");
-  res.duplicate_files.push_back(group1);
-  std::vector<std::string> group2;
-  group2.push_back(dir_name + "/readme.md");
-  group2.push_back(dir_name + "/docs/readme.txt");
-  group2.push_back(dir_name + "/x.y");
-  res.duplicate_files.push_back(group2);
 
-  return true;
+  std::vector<std::string> stack;
+  stack.push_back(dir_name.c_str());
+  while(!stack.empty()){
+    auto dirname = stack.back();
+    stack.pop_back();
+
+    printf("%s\n", dirname.c_str());
+
+    DIR * dir = opendir(dirname.c_str());
+    if(dir){
+      while(1){
+        dirent * de = readdir(dir);
+        if (!de) break;
+        std::string name = de -> d_name;
+        // n_files =+ 1;
+        if(name == "." || name == "..") continue;
+        std::string path = dirname + "/" + de -> d_name;
+        stack.push_back(path);
+      }
+      closedir(dir);
+    }
+  }
+
+
+
+
 }
