93872100ba9102d8b4e28868e2d40d6a86bbedfb
jiarong <jiarong.xu@ucalgary.ca>
2020-11-01 20:18:49 -0800

    detect file types

--- getDirStats.cpp
+++ getDirStats.cpp
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <unordered_map>
 
 static bool
 is_dir( const std::string & path)
@@ -18,20 +19,18 @@ is_dir( const std::string & path)
 bool
 getDirStats(const std::string & dir_name, Results & res)
 {
-
   res.largest_file_path; // path of the largest file in the directory
   res.largest_file_size = -1; // size of the largest file
   res.n_files = 0; // total number of files in the directory (recursive)
   res.n_dirs = 0; // total number of directories in the directory (recursive)
   res.all_files_size = 0; // cumulative size of all files
-  // res.most_common_types; // 5 most common file types, as reported by the file utility,
-  // res.duplicate_files; // list of groups of duplicate files (top 5)
+  res.most_common_types; // 5 most common file types, as reported by the file utility,
+  res.duplicate_files; // list of groups of duplicate files (top 5)
   struct stat st;
-  // if(stat(dir_name.c_str(), &st) != 0) {
-  // }
 
   std::vector<std::string> stack;
   stack.push_back(dir_name.c_str());
+
   while(!stack.empty()){
     auto dirname = stack.back();
     stack.pop_back();
@@ -47,8 +46,6 @@ getDirStats(const std::string & dir_name, Results & res)
 
         if(name == "." || name == "..") continue;
         std::string path = dirname + "/" + de -> d_name;
-        //printf("%s\n", path.c_str());
-        //printf("%s\n", name.c_str());
 
         if(stat(path.c_str(), &st) == 0){
             if( st.st_mode & S_IFDIR ){ //if directory
@@ -57,10 +54,6 @@ getDirStats(const std::string & dir_name, Results & res)
             }
             else if( st.st_mode & S_IFREG ){ //if file
 
-            // if (!IsIgnorable(name)){
-            //   allFiles.AddRange(Directory.GetFiles(folder, searchPattern, SearchOption.AllDirectories));
-            // }
-              //printf("%s\n", name.c_str());
               res.n_files += 1;
               int num = stat(path.c_str(), &st);
 
@@ -75,12 +68,39 @@ getDirStats(const std::string & dir_name, Results & res)
               }else{
                 //printf("%s\n", "file size error");
               }
+
+              std::string cmd = "file -b " + path; //***
+              FILE * fp = popen( cmd.c_str(), "r");
+              if(fp == nullptr){
+                printf("popen() failed, quitting\n");
+                exit(-1);
+              }
+
+              std::string ftype;
+              char buff[4096];
+              char * res = fgets(buff, sizeof(buff), fp);
+
+              if( res != nullptr){
+                int eol = 0;
+                while(buff[eol] != ',' && buff[eol] != '\n' && buff[eol] != 0) eol ++;
+                buff[eol] = 0;
+                ftype = buff;
+
+                printf("%s\n", ftype.c_str());
+
+              } else{
+                ftype = "file(1) failed, not sure why";
+              }
             }
             else{
             }
         }
-
         stack.push_back(path);
+
+        int n = 5;
+        std::unordered_map<std::string,int> hist;
+
+
       }
       closedir(dir);
     }
